73ebf6b80757238c669e43722fd9aff5f6538850
Felipe Norato <felipe.norato@wavy.global>
2019-09-02 23:39:24 -0300

    import project

--- tools/schematics/component-lib/files/__name@dasherize__.stories.ts
+++ tools/schematics/component-lib/files/__name@dasherize__.stories.ts
@@ -1,5 +1,6 @@
 import { storiesOf, moduleMetadata } from '@storybook/angular';
 import {<%= classify(name) %>Component} from './<%= dasherize(name) %>';
+import { <%= classify(project) %>ModuleName } from '@myorgworkspace/<%= dasherize(project) %>';
 
 const metadata = moduleMetadata({
   declarations: [
