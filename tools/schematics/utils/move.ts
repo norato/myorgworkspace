3719cd0cf80066a2868eae105ad1e9a808c6e6ef
Felipe Norato <felipe.norato@wavy.global>
2019-09-03 10:53:19 -0300

    Fix storybook erros on schematics template

--- tools/schematics/utils/move.ts
+++ tools/schematics/utils/move.ts
@@ -6,19 +6,37 @@ import {
   mergeWith,
   url,
   apply,
-  template
+  template,
+  forEach,
+  FileEntry,
+  when,
+  composeFileOperators
 } from '@angular-devkit/schematics';
 
-import { strings } from '@angular-devkit/core';
+import { strings, Path } from '@angular-devkit/core';
 
 export function moveTo(schema: Schema) {
+  const TEMPLATE_FILENAME_RE = /\.template$/;
   const source = url('./files');
   const rules: Rule[] = [
     template({
       ...strings,
       ...schema
     }),
-    move(schema.path)
+    move(schema.path),
+    forEach(
+      when(
+        path => path.endsWith('.template'),
+        composeFileOperators([
+          entry => {
+            return {
+              content: entry.content,
+              path: entry.path.replace(TEMPLATE_FILENAME_RE, '') as Path
+            };
+          }
+        ])
+      )
+    )
   ];
 
   return mergeWith(apply(source, rules));
